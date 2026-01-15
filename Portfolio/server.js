df8adab30664d34f274743b803ea4635d45c49ce
vishalbooraa <vishalboora00@gmail.com>
2026-01-15 20:12:08 +0530

    again fixed express static + vercel config

--- Portfolio/server.js
+++ Portfolio/server.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // Serve static files
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/static', express.static(path.join(__dirname, 'public')));
 
 
 // Nodemailer transporter configuration
