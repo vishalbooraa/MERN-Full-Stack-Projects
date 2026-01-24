eb9dfd40e2711f96356b691ab0e062cad0c4e755
vishalbooraa <vishalboora00@gmail.com>
2026-01-24 22:31:32 +0530

    created log in page

--- ConnectUp/frontend/src/pages/login/index.jsx
+++ ConnectUp/frontend/src/pages/login/index.jsx
@@ -1,12 +1,54 @@
 import UserLayout from '@/layout/UserLayout';
-import React from 'react';
+import { useRouter } from 'next/router';
+import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import styles from "./style.module.css";
 
 const LoginComponent = () => {
+
+    const loggedIn = useSelector(state => state.auth.loggedIn);
+    const router = useRouter();
+
+    const [userLoginMethod, setUserLoginMethod] = useState(true);
+
+    useEffect(() => {
+        if (loggedIn) {
+            router.push("/dashboard");
+        }
+    }, [loggedIn, router]);
+
     return (
         <UserLayout>
-            <p>Login page</p>
+            <div className={styles.container}>
+                <div className={styles.cardContainer}>
+                    <div className={styles.cardContainerLeft}>
+                        <p className={styles.cardLeftHeading}>
+                            {userLoginMethod ? "Sign In" : "Sign Up"}
+                        </p>
+
+                        <div className={styles.inputContainers}>
+                            <div className={styles.inputRow}>
+                                <input className={styles.inputField} type="text" placeholder='Username' />
+                                <input className={styles.inputField} type="text" placeholder='Name' />
+                            </div>
+
+                            <input className={styles.inputField} type="email" placeholder='Email' />
+                            <input className={styles.inputField} type="password" placeholder='Password' />
+
+                            <div
+                                className={styles.buttonWithOutline}
+                                onClick={() => setUserLoginMethod(!userLoginMethod)}
+                            >
+                                <p>{userLoginMethod ? "Sign In" : "Sign Up"}</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className={styles.cardContainerRight}></div>
+                </div>
+            </div>
         </UserLayout>
     );
-}
+};
 
 export default LoginComponent;
