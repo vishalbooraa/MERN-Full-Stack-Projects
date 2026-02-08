a308651d96074be57aaacb3f1e465c10aa14b922
vishalbooraa <vishalboora00@gmail.com>
2026-02-08 12:20:00 +0530

    created dashboard layout

--- ConnectUp/frontend/src/pages/dashboard/index.jsx
+++ ConnectUp/frontend/src/pages/dashboard/index.jsx
@@ -1,5 +1,6 @@
 import { getAboutUser } from '@/config/redux/action/authAction';
 import { getAllPosts } from '@/config/redux/action/postAction';
+import DashboardLayout from '@/layout/DashboardLayout';
 import UserLayout from '@/layout/UserLayout';
 import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
@@ -21,7 +22,7 @@ const Dashboard = () => {
             router.push("/login")
         }
         setIsTokenThere(true)
-    })
+    },[router])
 
     useEffect(()=>{
         if(isTokenThere){
@@ -32,20 +33,9 @@ const Dashboard = () => {
 
     return (
         <UserLayout>
-            <div className="container">
-                <div className="homeContainer">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
-                    </svg>
-
-                </div>
-                <div className="feedContainer">
-
-                </div>
-                <div className="extraContainer">
-
-                </div>
-            </div>
+            <DashboardLayout>
+                <h1>Dashboard</h1>
+            </DashboardLayout>
         </UserLayout>
     );
 }
